11dbdafe4d40db9f82f0efaca9996d81ee9d4ac5
KuberBhagat-7 <kuberbhagat7@gmail.com>
2023-12-15 10:01:44 +0000

    fundamental of js

---
+++ JS_FUNDAMENTAL/script.js
@@ -0,0 +1,7 @@
+let bday=10;
+let myname="Kuber Bhagat";
+console.log("MY birthday is on ",bday,"February","my name is",myname)
+
+let uni=210200100077;
+let branch="B.tech/C.S.E";
+console.log("My university roll no is",uni,"and branch is",branch)
