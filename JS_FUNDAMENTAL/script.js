0611023e9f6b70e094ba5eedd1b22d0dab8dd92f
KuberBhagat-7 <kuberbhagat7@gmail.com>
2023-12-15 10:11:42 +0000

    changes

--- JS_FUNDAMENTAL/script.js
+++ JS_FUNDAMENTAL/script.js
@@ -1,7 +1,16 @@
-let bday=10;
-let myname="Kuber Bhagat";
-console.log("MY birthday is on ",bday,"February","my name is",myname)
+// let bday=2;
+// let myname="Kuber Bhagat";
+// console.log("MY birthday is on ",bday,"February","my name is",myname)
 
-let uni=210200100077;
-let branch="B.tech/C.S.E";
-console.log("My university roll no is",uni,"and branch is",branch)
+// let uni=210200100077;
+// let branch="B.tech/C.S.E";
+// console.log("My university roll no is",uni,"and branch is",branch)
+
+// let s=10;
+// let t=20;
+// let result=s+t;
+// console.log("sum of s and t is",result)
+
+let myname="Kuber Bhagat";
+let Sum=4+5;
+console.log(`My name is ${myname}`,`My result is ${Sum}`)
